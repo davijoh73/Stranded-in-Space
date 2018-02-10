ec2be7c8ef3921723be6ad0d29426a89762dfb0d
davijoh73 <davetjohnston@hotmail.com>
2018-02-09 18:35:00 -0600

    feat: Created player object and added Navmesh agent to main camera. Also created navmesh for navigation on the bridge for testing on mobile device.

---
+++ Assets/Scripts/MoveToClickPoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToClickPoint : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            //Cast ray from center of camera viewport (essentially, the reticle point)
+            Vector3 reticle = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+
+            if (Physics.Raycast(reticle, Camera.main.transform.forward, out hit, 100.0f))
+            {
+                //Do not want to move when clicking on walls or certain items that are interactive
+                if (hit.collider.CompareTag("Item") || hit.collider.CompareTag("Wall"))
+                {
+                    agent.isStopped = true;
+                }
+                else
+                {
+                    agent.destination = hit.point;
+                    agent.isStopped = false;
+                }
+            }
+        }
+    }
+}
