1b5e5db53332b352dfe4426781a69593b387064d
davijoh73 <davetjohnston@hotmail.com>
2018-04-05 17:00:00 -0500

    feat: Add script to open crate in lab and sound effect to accompany the lid opening.

--- Assets/Scripts/OpenLabCrate.cs
+++ Assets/Scripts/OpenLabCrate.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class OpenLabCrate : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public bool crateOpen = false; //determine if crate is already open
+    public AudioSource soundSource; 
+    public AudioClip crateSound; //sound of the crate opening
+    public GameObject crateLid; //lid that will be rotated into open position
+    private float x; //variable used to rotate the lid
+
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (crateOpen && x < 90f)
+        {
+            x += Time.deltaTime * 45f;
+            crateLid.transform.rotation = Quaternion.Euler(x, 0, 0);
+        }
+    }
+
+    public void OnCrateClick()
+    {
+        crateOpen = true;
+        //Play audio clip of lid opening
+        soundSource.clip = crateSound;
+        soundSource.Play();
+
+    }
 }
