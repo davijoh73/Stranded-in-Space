5d32a21c1c523d05e5a40758b62e22f87077e9e8
davijoh73 <davetjohnston@hotmail.com>
2018-04-23 15:34:49 -0500

    feat: Add toilet and script to make it flush when button is pressed

---
+++ Assets/Scripts/ToiletFlush.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletFlush : MonoBehaviour
+{
+
+    public bool isFlushed = false; //determine if toilet has already been flushed
+    public AudioSource soundSource, buttonSoundSource;
+    public AudioClip soundEffect, buttonPressSound; //sound clip(s) to be used
+    public Material button1Dark, button2Lit;
+    public GameObject buttonObj, toiletFullStatus, toiletReadyStatus;
+    Material[] buttonMats;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Grab the materials for each object that need to be changed later
+        buttonMats = buttonObj.GetComponent<Renderer>().materials;
+    }
+
+    public void OnToiletFlush()
+    {
+        if (!isFlushed)
+        {
+            //Play button press sound
+            buttonSoundSource.clip = buttonPressSound;
+            buttonSoundSource.Play();
+
+            //Play audio clip w/ sound effect
+            soundSource.clip = soundEffect;
+            soundSource.Play();
+
+            //change materials for button to match new state
+            buttonMats[0] = button2Lit;
+            buttonMats[1] = button1Dark;
+            buttonObj.GetComponent<Renderer>().materials = buttonMats;
+
+            //Update the status screen on the Toilet console
+            toiletFullStatus.SetActive(false);
+            toiletReadyStatus.SetActive(true);
+        }
+        if (isFlushed)
+        {
+            //Play button press sound
+            buttonSoundSource.clip = buttonPressSound;
+            buttonSoundSource.Play();
+        }
+
+        isFlushed = true;
+
+    }
+}
