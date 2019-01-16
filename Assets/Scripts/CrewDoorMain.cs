f9aec8f6c447a18f45c8cd3118fd5ead94435314
davijoh73 <davetjohnston@hotmail.com>
2019-01-15 21:42:30 -0600

    Add mystery orb in crate in crew quarters and put door back for entry into crew quarters.

---
+++ Assets/Scripts/CrewDoorMain.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrewDoorMain : MonoBehaviour
+{
+    //Trigger used to open the door
+    public bool openDoor = false;
+    //Timer variable that counts down before door closes again
+    public static float doorTimer = 5;
+    //Sound clips used for when the door is opened
+    public AudioSource soundSource;
+    public AudioClip doorOpen;
+    //The two halves of the door that move when the door opens
+    public GameObject topHalf, bottomHalf;
+
+    void Update()
+    {
+        //If door is unlocked and not fully raised, continue raising door
+        if (openDoor && topHalf.transform.position.y < 1.34f)
+        {
+            topHalf.transform.Translate(0, 1.5f * Time.deltaTime, 0, Space.World);
+            bottomHalf.transform.Translate(0, -1.5f * Time.deltaTime, 0, Space.World);
+        }
+        //Once timer variable reaches zero, close door
+        else if (doorTimer <= 0 && topHalf.transform.position.y > .167)
+        {
+            topHalf.transform.Translate(0, -1.5f * Time.deltaTime, 0, Space.World);
+            bottomHalf.transform.Translate(0, 1.5f * Time.deltaTime, 0, Space.World);
+            //Play audio clip of door opening/closing (only play if it is not already playing, so that it only plays once during close sequence)
+            if (!soundSource.isPlaying)
+            {
+                soundSource.clip = doorOpen;
+                soundSource.Play();
+            }
+        }
+        //Decrement timer before door close and set openDoor variable back to false
+        else
+        {
+            doorTimer -= Time.deltaTime;
+            openDoor = false;
+        }
+
+    }
+
+    public void OnDoorClicked()
+    {
+        //Trigger the door to open via animation script and play opening door sound
+        openDoor = true;
+        doorTimer = 5; //reset door timer
+        //Play audio clip of door opening/closing
+        soundSource.clip = doorOpen;
+        soundSource.Play();
+
+    }
+
+
+}
---
+++ Assets/Scripts/OpenCrewCrate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCrewCrate : MonoBehaviour
+{
+
+    public bool crateOpen = false; //determine if crate is already open
+    public AudioSource soundSource;
+    public AudioClip crateSound; //sound of the crate opening
+    public GameObject crateLid; //lid that will be rotated into open position
+    private float x; //variable used to rotate the lid
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (crateOpen && x < 90f)
+        {
+            x += Time.deltaTime * 45f;
+            crateLid.transform.rotation = Quaternion.Euler(x, 0, 0);
+        }
+    }
+
+    public void OnCrateClick()
+    {
+        if (!crateOpen)
+        {
+            //Play audio clip of lid opening
+            soundSource.clip = crateSound;
+            soundSource.Play();
+        }
+
+        crateOpen = true;
+
+    }
+}
